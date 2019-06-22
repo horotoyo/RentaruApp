b4b14302c05c0a864037732ae59b0ff4ec9def0c
masuryo <suryowidiyantgm@gmail.com>
2019-06-22 14:36:03 +0700

    move wrapper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,9 @@
   <!-- =============================================== -->
 
   <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
   @yield('content')
+  </div>
   <!-- /.content-wrapper -->
 
   <!-- Dynamical Footer -->
